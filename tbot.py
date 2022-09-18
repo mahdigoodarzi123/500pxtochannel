6371c66a026f703bc455752c8554fdf1ea3bd096
mahdigoodarzi123 <erfangoodarzi123@gmail.com>
2022-09-18 02:28:20 -0700

    sending the downloaded pics to a channel

---
+++ tbot.py
@@ -0,0 +1,31 @@
+from time import sleep
+from pyrogram import Client, filters
+import os
+from fiveoopx import fivehundreddl
+
+# your api id and api hash
+bot = Client(
+    'ponsadpx',
+    #your api hash and your api id
+    api_id=,
+    api_hash=''
+)
+
+
+async def main():
+    async with bot:
+        for i in range(0,25):
+            # sending the picture to the channel
+            await bot.send_photo(chat_id="@your channel", photo=f'{i}.jpg')
+            print(f'[+] sent {i}.jpg')
+            # removing the picture after sending the photo
+            os.remove(f'{i}.jpg')
+
+try:
+    fivehundreddl()
+    sleep(5)
+    bot.run(main())
+
+except:
+    sleep(5)
+    bot.run(main())
